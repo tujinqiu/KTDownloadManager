37ce5fa904c702d0ebe982289538f338b83a70da
Kevin Tu <819931323@qq.com>
2016-08-15 19:36:57 +0800

    Initial Commit

---
+++ KTDownloadManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KTDownloadManager
+//
+//  Created by Kevin on 16/8/15.
+//  Copyright © 2016年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KTDownloadManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KTDownloadManager
+//
+//  Created by Kevin on 16/8/15.
+//  Copyright © 2016年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
